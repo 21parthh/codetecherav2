4bd63e6b5f78e7b8b29a60823482ef2df9810c40
Parth Deore <parth.deore@icloud.com>
2024-08-25 23:26:11 +0530

    feature added for services section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,11 +3,52 @@ import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 import Button from "./ui/Button";
 import { FaLocationArrow } from "react-icons/fa";
+import { FlipWords } from "./ui/FlipWords";
+import { HoverEffect } from "./ui/ServicesCard";
 
 const Hero = () => {
+	const words = ["better", "flexible", "beautiful", "modern"];
+	const projects = [
+		{
+			title: "Web Development",
+			description:
+				"A technology company that builds economic infrastructure for the internet.",
+			link: "https://stripe.com",
+		},
+		{
+			title: "Mobile App Development",
+			description:
+				"A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
+			link: "https://netflix.com",
+		},
+		{
+			title: "UI/UX Design",
+			description:
+				"A multinational technology company that specializes in Internet-related services and products.",
+			link: "https://google.com",
+		},
+		{
+			title: "Cloud Solutions",
+			description:
+				"A technology company that focuses on building products that advance Facebook's mission of bringing the world closer together.",
+			link: "https://meta.com",
+		},
+		{
+			title: "Artificial Intelligence & Machine Learning",
+			description:
+				"A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
+			link: "https://amazon.com",
+		},
+		{
+			title: "Custom Software Development",
+			description:
+				"A multinational technology company that develops, manufactures, licenses, supports, and sells computer software, consumer electronics, personal computers, and related services.",
+			link: "https://microsoft.com",
+		},
+	];
 	return (
 		<>
-			<div className="pb-20 pt-36">
+			<div className="pb-20 pt-30">
 				<Spotlight
 					className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
 					fill="white"
@@ -25,9 +66,9 @@ const Hero = () => {
 				{/* Radial gradient for the container to give a faded look */}
 				<div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
 			</div>
-			<div className="flex justify-center relative my-20 z-10">
+			<div className="flex flex-col items-center justify-center relative my-20 z-10">
 				<div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-					<h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80 pb-6">
+					<h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80 pb-2">
 						Transforming Ideas into Digital Reality.
 					</h2>
 					<TextGenerateEffect
@@ -43,6 +84,16 @@ const Hero = () => {
 						icon={<FaLocationArrow />}
 						position="right"
 					/>
+				</div>
+				<div className="h-[10rem] mt-28 flex justify-center items-center px-4">
+					<div className=" text-center text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
+						Build
+						<FlipWords words={words} /> <br />
+						software with CodeTechEra
+					</div>
+				</div>
+				<div className="max-w-5xl mx-auto px-8">
+					<HoverEffect items={projects} />
 				</div>
 			</div>
 		</>
